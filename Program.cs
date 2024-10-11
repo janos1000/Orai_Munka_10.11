59fefbefbdd22616a00283f085d8dd97a8f9b230
janos1000 <birinyij@kkszki.hu>
2024-10-11 12:03:31 +0200

    Az első feladat kész és működik.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace orai_munka_2.feladat
 {
@@ -10,12 +12,31 @@ namespace orai_munka_2.feladat
     {
         public static void Udvozol()
         {
-            Console.WriteLine(DateTime.Now);
+            int hour = DateTime.Now.Hour;
+            if (hour >= 5 && hour < 12)
+            {
+                Console.WriteLine("Jó reggelt.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("Jó napot.");
+            }
+            else if (hour >= 18 && hour < 4)
+            {
+                Console.WriteLine("Jó estét.");
+            }
+        }
+        public static void Bank()
+        {
+            Console.WriteLine("Hány éves?: ");
+            Console.WriteLine("Mennyi pénzt tesz be a bankba?: ");
+
+
 
         }
         static void Main(string[] args)
         {
-
+            Udvozol();
         }
     }
 }
