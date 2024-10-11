824c2eb5d3ffd493ece0c814aadf71330d70e30d
janos1000 <birinyij@kkszki.hu>
2024-10-11 11:32:15 +0200

    A feladat elkezdődött.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orai_munka_2.feladat
+{
+    internal class Program
+    {
+        public static void Udvozol()
+        {
+            Console.WriteLine(DateTime.Now);
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
